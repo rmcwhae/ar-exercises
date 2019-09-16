ec10a7ecf27c4f6d0f4b0baf53f1b2de171a547b
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-16 14:19:23 -0600

    Working solution to exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Stores.sum(:annual_revenue)
+puts Stores.average(:annual_revenue)
+puts Stores.where('annual_revenue >= 1000000').count
