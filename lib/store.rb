195a4c43371fd7eac76ccf00d44c40d64ce20888
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-16 21:31:02 -0600

    Disallows store destruction if store has employees

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Store < ActiveRecord::Base
   validate :must_carry_mens_or_womens_apparel
   # does the store carry at least one type of apparel?
@@ -8,4 +10,16 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  before_destroy :check_destroy_allowed, prepend: true
+
+  private
+
+  def check_destroy_allowed
+    # some condition that returns true or fails
+    if employees.count.positive?
+      errors[:base] << "Can't be destroyed because employees exist in this store"
+      throw :abort # necessary for Rails 5 per https://stackoverflow.com/questions/19044725/before-destroy-callback-not-stopping-record-from-being-deleted
+    end
+    employees.count ? false : true
+  end
 end
